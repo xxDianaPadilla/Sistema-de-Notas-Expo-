2d8847650a546600547b249ad1d14f7013dc03a9
Daniel Granados <20200008@ricaldone.edu.sv>
2024-12-06 10:14:20 -0600

    Cerrar sesion listo

---
+++ js/cerrarSesion.js
@@ -0,0 +1,21 @@
+/*Script para alerta de cerrar sesión*/
+const cerrarSesionBtn = document.querySelector('.cerrar-sesion');
+    const customAlert = document.getElementById('alert');
+    const btnConfirmar = document.getElementById('btnConfirmar');
+    const btnCancelar = document.getElementById('btnCancelar');
+
+    cerrarSesionBtn.addEventListener('click', () => {
+        customAlert.style.display = 'flex';
+    });
+
+    btnConfirmar.addEventListener('click', () => {
+        history.replaceState(null, null, '../index.html');
+        //Redirige al login
+        window.location.href = '../index.html';
+    });
+
+    btnCancelar.addEventListener('click', () => {
+    customAlert.style.display = 'none';
+});
+
+
